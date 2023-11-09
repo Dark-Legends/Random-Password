5bba4944a0bfaa19e3b2c3d48f1321d07e52b3a7
[♥] A <142109781+Dark-Legends@users.noreply.github.com>
2023-11-09 21:09:39 +0530

    Update Random-Passwords.sh

--- Random-Passwords.sh
+++ Random-Passwords.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-
-
-
-
 R="\033[1;31m"
 G="\033[1;32m"
 Y="\033[1;33m"
@@ -72,36 +67,27 @@ for ((i=1; i<=3; i++))
 do
 echo
 done
-     echo -e "\t${PI}Code ${W}By ${Y}Ethical ${G}Hacker ${B}Manjunath" 
+ echo -e "\t\a${PI}Code ${W}By ${B}Manjunath\n" 
 
 for ((i=1; i<=2; i++))
 do
 echo
 done
 echo
-echo -e "\t ${R}[${W}1${R}]  ${G}'8' ${Y}Letters Password"
-echo
-echo -e "\t ${R}[${W}2${R}]  ${G}'10' ${Y}Letters Password"
-echo
-echo -e "\t ${R}[${W}3${R}]  ${G}'12' ${Y}Letters Password"
-echo
-echo -e "\t ${R}[${W}4${R}]  ${G}'14' ${Y}Letters Password"
-echo
-echo -e "\t ${R}[${W}5${R}]  ${G}'16' ${Y}Letters Password"
-echo
-echo -e "\t ${R}[${W}6${R}]  ${G}'18' ${Y}Letters Password"
-echo
-echo -e "\t ${R}[${W}7${R}]  ${G}'20' ${Y}Letters Password"
-echo
-echo -e "\t ${R}[${W}8${R}]  ${Y}Go-Back"
-echo
-
+echo -e "\t ${R}[${W}1${R}]  ${G}'8' ${Y}Letter Password\n"
+echo -e "\t ${R}[${W}2${R}]  ${G}'10' ${Y}Letter Password\n"
+echo -e "\t ${R}[${W}3${R}]  ${G}'12' ${Y}Letter Password\n"
+echo -e "\t ${R}[${W}4${R}]  ${G}'14' ${Y}Letter Password\n"
+echo -e "\t ${R}[${W}5${R}]  ${G}'16' ${Y}Letter Password\n"
+echo -e "\t ${R}[${W}6${R}]  ${G}'18' ${Y}Letter Password\n"
+echo -e "\t ${R}[${W}7${R}]  ${G}'20' ${Y}Letter Password\n"
+echo -e "\t ${R}[${W}8${R}]  ${Y}Go-Back\n"
 for ((i=1; i<=3; i++))
 do
 echo
 done
 echo -e "\e[38;5;93m"
-read -p "Boss Enter The Password Letters  : " Start
+read -p "Boss Enter The Password Length  : " Start
 
 clear
 for ((i=1; i<=5; i++))
@@ -110,17 +96,16 @@ echo
 done
 
 case $Start in
-    1) passwords=$(cat /dev/urandom | tr -dc 'a-zA-Z$0-9*@' | head -c 8) && echo -e "\tBoss Your '8' ${R}Letters Password : ${G}" $passwords ;;
-    2) passwords=$(cat /dev/urandom | tr -dc '*@a-z$A-Z0-9' | head -c 10) && echo -e "\tBoss Your '10' ${R}Letters Password : ${G}" $passwords ;;
-    3) passwords=$(cat /dev/urandom | tr -dc 'a-z$*@A-Z0-9' | head -c 12) && echo -e "\tBoss Your '12' ${R}Letters Password : ${G}" $passwords ;;
-    4) passwords=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9@$*' | head -c 14) && echo -e "\tBoss Your '14' ${R}Letters Password : ${G}" $passwords ;;
-    5) passwords=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9@$*' | head -c 16) && echo -e "\tBoss Your '16' ${R}Letters Password : ${G}" $passwords ;;
-    6) passwords=$(cat /dev/urandom | tr -dc 'a-zA-Z@$*0-9' | head -c 18) && echo -e "\tBoss Your '18' ${R}Letters Password : ${G}" $passwords ;;
-    7) passwords=$(cat /dev/urandom | tr -dc '@a-zA-Z*0-9$' | head -c 20) && echo -e "\tBoss Your '20' ${R}Letters Password : ${G}" $passwords ;;
+    1) passwords=$(cat /dev/urandom | tr -dc 'a-zA-Z$0-9*@' | head -c 8) && echo -e "\tBoss Your '8' ${R}Password Length : ${G}" $passwords ;;
+    2) passwords=$(cat /dev/urandom | tr -dc '*@a-z$A-Z0-9' | head -c 10) && echo -e "\tBoss Your '10' ${R}Password Length : ${G}" $passwords ;;
+    3) passwords=$(cat /dev/urandom | tr -dc 'a-z$*@A-Z0-9' | head -c 12) && echo -e "\tBoss Your '12' ${R}Password Length : ${G}" $passwords ;;
+    4) passwords=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9@$*' | head -c 14) && echo -e "\tBoss Your '14' ${R}Password Length : ${G}" $passwords ;;
+    5) passwords=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9@$*' | head -c 16) && echo -e "\tBoss Your '16' ${R}Password Length : ${G}" $passwords ;;
+    6) passwords=$(cat /dev/urandom | tr -dc 'a-zA-Z@$*0-9' | head -c 18) && echo -e "\tBoss Your '18' ${R}Password Length : ${G}" $passwords ;;
+    7) passwords=$(cat /dev/urandom | tr -dc '@a-zA-Z*0-9$' | head -c 20) && echo -e "\tBoss Your '20' ${R}Password Length : ${G}" $passwords ;;
     8) exit ;;
     *) echo -e "\t${R} Error Please choose  " 
 esac
-
 
 for ((i=1; i<=5; i++))
 do
@@ -136,8 +121,7 @@ echo
         break
     elif [ "$No" == "no" ]; then
 		echo
-        echo -e "${G}Thank ${PI}You ${Y}All"
-	echo
+        echo -e "\a${G}Thank ${PI}You ${Y}All\n"
         break
     else
         echo -e "${R}Error: Invalid ${Y}Input. Please Enter ${G}'yes' ${W}or ${G}'no'"
